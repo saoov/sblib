5358a220a501cefbe35651c0d62f0154dd8012f7
saoov <68726498+saoov@users.noreply.github.com>
2021-03-09 15:51:49 +0900

    Delete sblib directory

--- sblib/src/main/webapp/resources/js/main.js
+++
@@ -1,7 +0,0 @@
-$(".menus ul li").on("click",function() {
-    $(this).children("ul").stop().toggle()
-});
-$(".addbooks").on("click", function(){
-    window.open('selectBook.html', '네이버팝업', 'width=700px,height=800px,scrollbars=no');
-});
-
